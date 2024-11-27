f5f262c62d9b9ea2850f88a34093d939f64652e7
Christopher Langenbahn <langenbahn.11@osu.edu>
2024-11-27 14:14:49 -0500

    forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_22_191129) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_27_185931) do
+  create_table "expense_participants", force: :cascade do |t|
+    t.integer "expense_id", null: false
+    t.integer "user_id", null: false
+    t.decimal "share", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_expense_participants_on_expense_id"
+    t.index ["user_id"], name: "index_expense_participants_on_user_id"
+  end
+
   create_table "expenses", force: :cascade do |t|
     t.string "description"
     t.decimal "amount"
@@ -62,6 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_22_191129) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expense_participants", "expenses"
+  add_foreign_key "expense_participants", "users"
   add_foreign_key "expenses", "trips"
   add_foreign_key "expenses", "users", column: "creator_id"
   add_foreign_key "participants", "trips"
