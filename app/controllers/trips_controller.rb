5fa9d01e49c8842f3a6fa84dbdad1f89718a3aea
Aysha Chaudhry <aysha45458@gmail.com>
2024-11-29 17:49:18 -0500

    Trip creation controller.

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,36 @@
+class TripsController < ApplicationController
+  before_action :authenticate_user! # only logged-in users can create trips
+
+  def new
+    @trip = Trip.new
+    @users = User.where.not(id: current_user.id) # current user is not a participant
+  end
+
+  def create
+    @trip = current_user.trips.build(trip_params)
+    if @trip.save
+      add_participants(@trip, params[:trip][:participant_ids])
+      redirect_to @trip, notice: 'Trip created!'
+    else # trip could not be created
+      @users = User.where.not(id: current_user.id) # reloads list of users to try again
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :description, :start_date, :end_date)
+  end
+
+  # given trip and participants' user_ids, add participants to trip
+  def add_participants(trip, participant_ids)
+    participant_ids.reject(&:blank?).each do |user_id|
+      trip.participants.create(user_id: user_id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/login', to: 'session#new', as: 'login' # route to show log in form
-  post '/login', to: 'sessions#create' # route to handle login submission
-  delete '/logout', to: 'sessions#destroy', as: 'logout' # route to handle logout
+  resources :trips, only: %i[new create show] # routes for creating a trip
   # Defines the root path route ("/")
   root 'home#index'
 end
