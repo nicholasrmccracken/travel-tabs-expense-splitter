90e36d7fc301da7c7f5e2c752dbedb6bde4359d8
Aysha Chaudhry <aysha45458@gmail.com>
2024-11-29 17:30:04 -0500

    Login/out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/login', to: 'session#new', as: 'login' # route to show log in form
+  post '/login', to: 'sessions#create' # route to handle login submission
+  delete '/logout', to: 'sessions#destroy', as: 'logout' # route to handle logout
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
 end
