de632a31d060cd2b4ea3ec7d2649c5edbb2279d3
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2024-12-01 16:10:40 -0500

    Resolve custom colors not appearing

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,6 +21,17 @@ module.exports = {
 
         'button-red': '#ef4444',
         'button-hover-red': '#b91c1c',
+
+        dark_logo: '#5e2807',
+        light_logo: '#f2dfc9',
+  
+        red_accent: '#BF0000',
+        green_accent: '#64C7A0',
+
+        off_white: '#d0b8b2',
+        light_brown: '#AD7064',
+        medium_brown: '#7B554D',
+        dark_brown: '#2E0B05',
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
