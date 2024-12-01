fd9d2b16aa105dd0ab8166fc755f4df162613947
Christopher Langenbahn <langenbahn.11@osu.edu>
2024-12-01 11:54:02 -0500

    styled new trip page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'purple': '#3f3cbb',
+        'primary-red': '#991b1b',
+        'secondary-red': '#f87171',
+  
+        'primary-grey': '#57534e',
+        'secondary-grey': '#d4d4d4',
+  
+        'background-red': '#fee2e2',
+        'background-grey': '#e5e7eb',
+
+        'button-red': '#ef4444',
+        'button-hover-red': '#b91c1c',
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
