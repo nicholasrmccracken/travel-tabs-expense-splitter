2c47538927c089cd949d9fd015318e8da94a7faa
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2024-12-01 14:06:58 -0500

    Resolve tailwind compilation error and initialize expenses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :trips, only: %i[index show new create destroy] do
+  resources :trips do
     member do
       delete :leave
     end
+
+    resources :expenses
   end
 
   root 'trips#index'
