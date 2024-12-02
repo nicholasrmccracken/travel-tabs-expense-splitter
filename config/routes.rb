a9923cc9d76c1fe09887b797d4a103fdf0df7a1a
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2024-12-01 23:30:12 -0500

    Hotfix for expense leave path

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   get 'pages/about'
   devise_for :users
 
-
   get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   get '/about', to: 'pages#about', as: 'about'
 
-  resources :trips, only: %i[index show new create edit update destroy] do
+  resources :trips do
+    resources :expenses do
+      post 'leave', on: :member, to: 'expenses#leave', as: 'leave'
+    end
+
     member do
       post :leave
     end
@@ -14,4 +17,3 @@ Rails.application.routes.draw do
 
   root 'trips#index'
 end
-
