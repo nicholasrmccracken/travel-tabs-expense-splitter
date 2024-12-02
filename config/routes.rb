2d6ff7eac01d4176baa7625ef08704e8f7c06805
Sanju Nandimalla <sanjunandimalla@Sanjus-MacBook-Pro.local>
2024-12-01 23:40:53 -0500

    Updates navbar and added logo/Expenses button

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,10 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   get '/about', to: 'pages#about', as: 'about'
 
-  resources :trips, only: %i[index show new create edit update destroy] do
-    member do
-      post :leave
-    end
+  resources :trips do
+    resources :expenses, only: %i[index show new create edit update destroy]
   end
+  
 
   root 'trips#index'
 end
