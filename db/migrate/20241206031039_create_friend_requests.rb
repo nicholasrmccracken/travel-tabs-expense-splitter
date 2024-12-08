47c2b0e87859a9f7da4afa6fa2475e500f9be321
Sanju Nandimalla <sanjunandimalla@Sanjus-MacBook-Pro.local>
2024-12-07 23:31:11 -0500

    Troubleshooting users page

--- db/migrate/20241206031039_create_friend_requests.rb
+++ db/migrate/20241206031039_create_friend_requests.rb
@@ -1,9 +1,9 @@
 class CreateFriendRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :friend_requests do |t|
-      t.references :sender, null: false, foreign_key: true
-      t.references :receiver, null: false, foreign_key: true
-      t.string :status
+      t.references :sender, foreign_key: { to_table: :users }, null: false
+      t.references :receiver, foreign_key: { to_table: :users }, null: false
+      t.string :status, default: 'pending'
 
       t.timestamps
     end
